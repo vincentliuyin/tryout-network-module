f1ce5d24092d28946095fc0f24808b9d7fe44f04
vincentliuyin <vincent.liuyin@gmail.com>
2024-08-06 16:03:47 +0200

    Update main.tf
    Add double quote to the values

--- main.tf
+++ main.tf
@@ -2,18 +2,18 @@ module "network_vpc_1" {
   source  = "app.terraform.io/vincent-test-gcp/networkvincent1/google"
   version = "1.0.0"
 
-  project  = vincentliu-sandbox
-  region   = europenorth-1
-  vpc_name = test-vpc1
+  project  = "vincentliu-sandbox"
+  region   = "europenorth-1"
+  vpc_name = "test-vpc1"
 }
 
 module "network_vpc_2" {
   source  = "app.terraform.io/vincent-test-gcp/networkvincent1/google"
   version = "1.0.0"
 
-  project  = vincentliu-sandbox
-  region   = europenorth-1
-  vpc_name = test-vpc2
+  project  = "vincentliu-sandbox"
+  region   = "europenorth-1"
+  vpc_name = "test-vpc2"
 }
 
 
