e39d01b97fcfe3fbac9d6b82520c22c979da1e89
vincentliuyin <vincent.liuyin@gmail.com>
2024-08-06 17:23:23 +0200

    Update main.tf
    remove 1 vpc

--- main.tf
+++ main.tf
@@ -7,13 +7,5 @@ module "network_vpc_1" {
   vpc_name = "test-vpc1"
 }
 
-module "network_vpc_2" {
-  source  = "app.terraform.io/vincent-test-gcp/networkvincent1/google"
-  version = "1.0.3"
-
-  project  = "vincentliu-sandbox"
-  region   = "europe-north1"
-  vpc_name = "test-vpc2"
-}
 
 
