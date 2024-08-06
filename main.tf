0c558dc7c69195adae8a0c95bcfbff7880ce02d5
vincentliuyin <vincent.liuyin@gmail.com>
2024-08-06 16:24:54 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ module "network_vpc_1" {
 
 module "network_vpc_2" {
   source  = "app.terraform.io/vincent-test-gcp/networkvincent1/google"
-  version = "1.0.1"
+  version = "1.0.2"
 
   project  = "vincentliu-sandbox"
   region   = "europenorth-1"
